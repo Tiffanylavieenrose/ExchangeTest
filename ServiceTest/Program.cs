c8d4e36ab80370658e584a5f1f17be1d743ef0a2
Tiffanylavieenrose <tiffanylove920@gmail.com>
2019-10-05 18:11:53 +0800

    加入專案檔案。

---
+++ ServiceTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DTO.ReqInParm;
+using ExchangeService;
+
+namespace ServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IMainService mainservice = new MainServiceClient();
+            DTO.ReqInParm.CCertificateSpcReqInParm parm1 = new DTO.ReqInParm.CCertificateSpcReqInParm();
+            Console.WriteLine("Hello World!");
+        }
+
+       
+    }
+}
